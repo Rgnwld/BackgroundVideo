451e1335a8a04bb9bb04eb6656a27b65f18d439b
Reginaldo <Regifilho06@hotmail.com>
2020-09-26 15:20:48 -0300

    Updates

---
+++ precache-manifest.7dc8cd35d982dfcbb9b2d9b56bf205f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04554829c1a5b02e6d0fb46e79d02549",
+    "url": "/BackgroundVideo/index.html"
+  },
+  {
+    "revision": "3167abafa6bd601ff960",
+    "url": "/BackgroundVideo/static/css/main.40ce4fa0.chunk.css"
+  },
+  {
+    "revision": "ba2f21d8a31d63fb9679",
+    "url": "/BackgroundVideo/static/js/2.d9551213.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BackgroundVideo/static/js/2.d9551213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3167abafa6bd601ff960",
+    "url": "/BackgroundVideo/static/js/main.37922ad9.chunk.js"
+  },
+  {
+    "revision": "45ae4a755695d942dba7",
+    "url": "/BackgroundVideo/static/js/runtime-main.9ce94d77.js"
+  },
+  {
+    "revision": "1cc993a3ad8e2bee1517a4b1685211a8",
+    "url": "/BackgroundVideo/static/media/airplane.1cc993a3.svg"
+  },
+  {
+    "revision": "ecef733a33ceec8dc32d29510998afe9",
+    "url": "/BackgroundVideo/static/media/loupe.ecef733a.svg"
+  }
+]);
